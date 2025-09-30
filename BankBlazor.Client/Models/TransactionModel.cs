69ccc7966bab0ed0b2c97a1b86a6b3da9c613e53
joel abraha <joel.abraha95@gmail.com>
2025-09-30 16:10:29 +0200

    did razor and model transaction.

---
+++ BankBlazor.Client/Models/TransactionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BankBlazor.Client.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
