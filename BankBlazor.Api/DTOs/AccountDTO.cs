9920643167eb847bc699004cc6bce3f7d66f291d
joel abraha <joel.abraha95@gmail.com>
2025-10-06 02:08:03 +0200

    Did DTOs folder and classes

---
+++ BankBlazor.Api/DTOs/AccountDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Transactions;
+
+namespace BankBlazor.Api.DTOs
+{
+    public class AccountDto
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; } = string.Empty;
+
+        public List<TransactionDto> Transactions { get; set; } = new();
+    }
+}
